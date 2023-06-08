0286738ba00b4aa860ea5cad942340d32c2b29fd
Chandan-Chandra-shil <chandanchandrashil22@gmail.com>
2023-06-08 16:21:05 +0600

    createpopular  instructor api

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const express = require("express")
-const cors = require("cors")
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
-
-
-
+app.use(cors());
+app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dyntprt.mongodb.net/?retryWrites=true&w=majority`;
@@ -27,18 +24,21 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-      const usersCollection = client.db("tuneTimeDB").collection("users")
-      const classesCollection = client.db("tuneTimeDB").collection("classes")
-  
-    
-    // get all data api
+    const usersCollection = client.db("tuneTimeDB").collection("users");
+    const classesCollection = client.db("tuneTimeDB").collection("classes");
+
+    // classes related api
     app.get("/all-class", async (req, res) => {
       const result = await classesCollection.find().toArray();
-        res.send(result)
-    })
+      res.send(result);
+    });
 
+    // users related api
 
-
+    app.get("/all-instructor", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -52,13 +52,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('tune time server is running ')
-})
+app.get("/", (req, res) => {
+  res.send("tune time server is running ");
+});
 
 app.listen(port, () => {
-  console.log(`tune time server is running on the PORT ${port}`)
-})
+  console.log(`tune time server is running on the PORT ${port}`);
+});
