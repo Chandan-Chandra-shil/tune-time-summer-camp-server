dff74018d10d76b1d2398624677f398fd99bd966
Chandan-Chandra-shil <chandanchandrashil22@gmail.com>
2023-06-11 12:01:02 +0600

    create make a admin api

--- index.js
+++ index.js
@@ -31,7 +31,9 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const usersCollection = client.db("tuneTimeDB").collection("users");
-    const instructorsCollection = client.db("tuneTimeDB").collection("instructors");
+    const instructorsCollection = client
+      .db("tuneTimeDB")
+      .collection("instructors");
     const classesCollection = client.db("tuneTimeDB").collection("classes");
     const selectedClassesCollection = client
       .db("tuneTimeDB")
@@ -42,35 +44,50 @@ async function run() {
       const result = await classesCollection.find().toArray();
       res.send(result);
     });
-    
-    // classes  post api 
-     app.post("/all-class", async (req, res) => {
-       const newClass = req.body;
-       const result = await classesCollection.insertOne(newClass);
-       res.send(result);
-     });
 
-    // users get api
+    // classes  post api
+    app.post("/all-class", async (req, res) => {
+      const newClass = req.body;
+      const result = await classesCollection.insertOne(newClass);
+      res.send(result);
+    });
 
     app.get("/all-instructors", async (req, res) => {
       const result = await instructorsCollection.find().toArray();
       res.send(result);
     });
 
-    // users post api
+    //users get api
+    app.get("/all-users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
 
+    // users post api
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
       const existingUser = await usersCollection.findOne(query);
-
       if (existingUser) {
         return res.send({ message: "user already exists" });
       }
-
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
+
+    // users admin api 
+    app.patch('/all-users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result)
+
+    })
 
     // selectedClasses get api
     app.get("/all-selectedClasses", async (req, res) => {
